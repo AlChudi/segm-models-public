5328dad4e538130cc44f2bd785fae3130d2be059
AlChudi <147726877+AlChudi@users.noreply.github.com>
2024-04-11 13:30:48 +0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -118,8 +118,8 @@ class SegmentationTrainer:
             dict(params=self._model.parameters(), lr=learning_rate),
         ])
 
-        self._scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self._optimizer, self.epochs_count)
-        # self._scheduler = torch.optim.lr_scheduler.StepLR(self._optimizer, step_size=10, gamma=0.9)
+        # self._scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self._optimizer, self.epochs_count)
+        self._scheduler = torch.optim.lr_scheduler.StepLR(self._optimizer, step_size=10, gamma=0.9)
 
         self._train_epoch = smp_utils.train.TrainEpoch(
             self._model,
